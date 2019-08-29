823c0be258cfc4fa899b89bc86d9939ffea145d1
Björn Alfsson <Bjorn.KristenssonAlfsson@alten.se>
2019-08-29 14:12:33 +0200

    Cards rendered out.

--- src/components/gallery/GalleryCard.js
+++ src/components/gallery/GalleryCard.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import './GalleryCard.css';
 
 const GalleryCard = props => (
     <div>
     {props.data.length > 0 ? (
-        console.log(props.data) && <p>Bla</p>
-            ) : <p>Nothing stored in the database</p>
+        props.data.map( card => (
+            <div className="thumbnail" key={card.id}>
+            <p>{card.title}</p>
+            {card.media_type == 'image' && <img src={card.hdurl} />}
+            {card.media_type == 'video' && <iframe className="iframe" width="70%" src={card.url}></iframe>}
+            </div>
+            ))) : <p>Nothing stored in the database</p>
     }
     </div>
 )
